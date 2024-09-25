d6cf0c6e8154b503072aa0173d0affd8f283292e
Nawrashawari <Nawrashawari@gmail.com>
2024-09-25 19:12:30 -0400

    Initial Commit

---
+++ VapeRator/VapeRatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VapeRatorApp.swift
+//  VapeRator
+//
+//  Created by Nawras Hawari on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct VapeRatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
